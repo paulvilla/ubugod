e68ceffe71da5b4f5066aa62fe48ba136afcbf39
Paul Villa <pablovillarejo90@gmail.com>
2017-07-31 17:28:25 +0200

    Fixed

--- install.sh
+++ install.sh
@@ -100,13 +100,11 @@ then
         echo "${blue}Instalando herramientas de Pentesting...${NC}";
     sleep 3s;
             apt install subversion -y
-            apt install build-essential libssl-dev libnl-3-dev sqlite3 libsqlite3-dev libnl-genl-3-dev libpcap-dev -y
+            apt install build-essential libssl-dev libnl-3-dev sqlite3 libsqlite3-dev libnl-genl-3-dev libpcap-dev g++ dpkg-config -y
             apt install aircrack-ng reaver -y
         cd /root/ubugod/modulos/pentesting/aircrack-ng-1.2-rc4/
-            make
-            make install
-            make sqlite=true
-            make sqlite=true install
+            make sqlite=true experimental=true ext_scripts=true
+	    make sqlite=true experimental=true ext_scripts=true install
             airodump-ng-oui-update
         cd scripts/airoscript-ng
             make
